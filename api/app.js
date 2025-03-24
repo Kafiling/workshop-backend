1adfda8b947979e9b52c13b6954fbcc2feefbbd1
Kafiling <panthawit.k@gmail.com>
2025-03-24 19:44:26 +0700

    npm init + install package

--- api/app.js
+++ api/app.js
@@ -1,2 +1,13 @@
-const y = require("./myModule");
-y.x();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(5001, (err) => {
+  console.log("Server is running on port 5001");
+  if (err) {
+    console.log("Error : " + err);
+  }
+});
